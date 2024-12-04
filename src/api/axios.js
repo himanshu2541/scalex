aff341b9cba7945172460de81c07c16e302439a3
Himanshu Singh <hsksingh20072005@gmail.com>
2024-12-04 12:40:11 +0530

    changed axios config

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,4 +4,8 @@ const BASE_URL = "https://scalex-backend.vercel.app/";
 
 export default axios.create({
   baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
 });
