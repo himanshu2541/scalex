15fa6245879126109937b29b9f51dfaf81d3e43d
Himanshu Singh <hsksingh20072005@gmail.com>
2024-12-05 09:53:31 +0530

    changed the backend domain

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://scalex-backend.vercel.app/";
+const BASE_URL = "https://scalex.onrender.com/";
 
 export default axios.create({
   baseURL: BASE_URL,
